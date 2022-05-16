f03c8312c779b47da70be948780cfe6b747f7bc4
Bela Terjek <bela@netc.hu>
2022-05-16 23:08:14 +0200

    Test routine for the keycap illuminator leds

--- test.ino
+++ test.ino
@@ -6,10 +6,28 @@
 
 //setup routine
 void setupTest() {
+  ioLeds.begin();
+  ioLeds.show();
+  ioLeds.setBrightness(16);
 
+  #if defined(NEO_PWR)
+    pinMode(NEO_PWR, OUTPUT);
+    digitalWrite(NEO_PWR, true);
+  #endif
 }
 
 //main loop
 void loopTest() {
+  testIoNeo(20);
+}
 
+void testIoNeo(int wait) {
+  for(long firstHue = 0; firstHue < 5*65536; firstHue += 256) {
+    for(int i=0; i<ioLeds.numPixels(); i++) {
+      int pixelHue = firstHue + (i * 65536L / ioLeds.numPixels());
+      ioLeds.setPixelColor(i, ioLeds.gamma32(ioLeds.ColorHSV(pixelHue)));
+    }
+    ioLeds.show();
+    delay(wait);
+  }
 }
